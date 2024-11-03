470df0a167220fcffc183fb5c693e1fd794993a8
yvesleciel <yvangoudjou04@gmail.com>
2024-11-03 17:28:44 +0100

    coaching angular: input/output

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import {UsersComponent} from "./users/users.component";
+import {UserDetailsComponent} from "./user-details/user-details.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'users',
+    component: UsersComponent,
+  },
+  {
+    path: 'users/:id',
+    component: UserDetailsComponent
+  },
+  {
+    path: '',
+    redirectTo: 'users', pathMatch: 'full',
+  }
+];
